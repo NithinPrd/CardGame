b8a1bb277505a63674f953c05732f14432e458a0
Nithin Pradeep <nitin.prdp@gmail.com>
2022-01-29 16:33:27 +0530

    Initial Commit

---
+++ cardgame/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  cardgame
+//
+//  Created by Nithin Pradeep on 29/01/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ cardgame/cardgameApp.swift
@@ -0,0 +1,17 @@
+//
+//  cardgameApp.swift
+//  cardgame
+//
+//  Created by Nithin Pradeep on 29/01/22.
+//
+
+import SwiftUI
+
+@main
+struct cardgameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
