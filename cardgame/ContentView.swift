802592eaba5a7e03adbf79751e5259c43fb62e29
Nithin Pradeep <nitin.prdp@gmail.com>
2022-01-29 16:53:48 +0530

    Fixed Background Color

--- cardgame/ContentView.swift
+++ cardgame/ContentView.swift
@@ -9,41 +9,49 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Spacer()
-            Image("logo")
-            Spacer()
-            HStack {
+        ZStack {
+            Image("background")
+                .ignoresSafeArea()
+            VStack {
                 Spacer()
-                Image("card2")
+                Image("logo")
                 Spacer()
-                Image("card3")
-                Spacer()
-            }
-            Spacer()
-            Image("dealbutton")
-            Spacer()
-            HStack {
-                Spacer()
-                VStack() {
-                    Text("Player")
-                        .foregroundColor(Color.white)
-                        .padding(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
-                    Text("0")
-                        .foregroundColor(Color.white)
-                        
+                HStack {
+                    Spacer()
+                    Image("card2")
+                    Spacer()
+                    Image("card3")
+                    Spacer()
                 }
                 Spacer()
-                VStack() {
-                    Text("CPU")
-                        .foregroundColor(Color.white)
-                        .padding(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
-                    Text("0")
-                        .foregroundColor(Color.white)
+                Image("dealbutton")
+                Spacer()
+                HStack {
+                    Spacer()
+                    VStack() {
+                        Text("Player")
+                            .font(.subheadline)
+                            .foregroundColor(Color.white)
+                            .padding(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
+                        Text("0")
+                            .font(.largeTitle)
+                            .foregroundColor(Color.white)
+                            
+                    }
+                    Spacer()
+                    VStack() {
+                        Text("CPU")
+                            .font(.subheadline)
+                            .foregroundColor(Color.white)
+                            .padding(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
+                        Text("0")
+                            .font(.largeTitle)
+                            .foregroundColor(Color.white)
+                    }
+                    Spacer()
                 }
                 Spacer()
             }
-            Spacer()
         }
     }
 }
@@ -52,6 +60,5 @@ struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
             .previewInterfaceOrientation(.portrait)
-            .background(/*@START_MENU_TOKEN@*//*@PLACEHOLDER=View@*/Color.green/*@END_MENU_TOKEN@*/).ignoresSafeArea()
     }
 }
